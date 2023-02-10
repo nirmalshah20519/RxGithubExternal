60cd73b0fa99a3e698f744507d906d2a7b508510
nirmalshah20519 <nirmalshah20519@gmail.com>
2023-02-10 11:35:51 +0530

    index4.php created

---
+++ index4.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ultricies erat vitae metus congue pulvinar. Fusce aliquam pellentesque tincidunt. Praesent et consectetur mauris, ut tempus diam. Aenean nec faucibus lectus, id interdum metus. Aenean tempor dignissim ante id laoreet. Pellentesque egestas metus orci, eget ullamcorper est feugiat convallis. Nam ullamcorper eros sit amet facilisis venenatis. Donec dignissim iaculis iaculis. Cras hendrerit ac magna a rutrum. Quisque condimentum rutrum nunc, sit amet accumsan diam ullamcorper ut.
+</p>
+</body>
+</html>
